39766d694869bee097d9440b065b1a5d621c5c58
cubewebs <juanjoseaos@hotmail.com>
2024-02-16 09:52:24 +0000

    first commit

---
+++ src/models/server.ts
@@ -0,0 +1,20 @@
+import express, { Application } from 'express';
+
+class Server {
+  private app: Application;
+  private port: string;
+
+  constructor() {
+    this.app = express();
+    this.port = '3001';
+    this.listen();
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`App is running on port ${this.port}`);
+    });
+  }
+}
+
+export default Server;
